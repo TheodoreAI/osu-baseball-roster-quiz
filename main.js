9a538a71ea73d80ac9a354231c4ecb37c98c2c2b
Mateo Estrada <mateoestrada@estradma-mbp16.home>
2022-02-01 15:57:36 -0800

    Added a new function to change the color of the bases based on if the user chooses right or wrong.

--- main.js
+++ main.js
@@ -2,29 +2,63 @@
 var helloWorld = "Baseball Quiz";
 var positions = ['Catcher', 'Pitcher', 'First Base', 'Second Base',  'Third Base', 'Shortstop', 'Right Field', 'Center Field', 'Left Field'];
 
-// HEADER Section
-var header = document.getElementById("homeHeader");
-header.textContent = helloWorld;
 
-var myButton = document.querySelector("button");
-myButton.onclick = () => {
+
+// Begins the process
+window.onload = () => {
     beginQuiz();
     addCircleEvents();
 }
 
 
-// Game Section
+// HEADER Section
+var header = document.getElementById("homeHeader");
+header.textContent = helloWorld;
+
+var myButton = document.querySelectorAll("button")[0];
+myButton.onclick = () => {
+    beginQuiz();
+    addCircleEvents();
+}
+
+var myButton = document.querySelectorAll("button")[1];
+myButton.onclick = () => {
+    window.location.reload();
+}
+
+
+
+// Function Section
 function beginQuiz(){
     var quizQuestions = document.querySelector("p");
     quizQuestions.textContent = positions[Math.floor(Math.random()*positions.length)];
     return quizQuestions.textContent;
 }
 
+function changeColor(tag, color){
+    console.log(tag.firstElementChild);
+    tag.firstElementChild.style.fill = `${color}`;
+}
+
 function addCircleEvents(){
-    var circleList = document.querySelectorAll("circle");
-    circleList.forEach((circle)=>{
-        circle.setAttribute("onclick", "checkAnswer()");
+    var positionTags = [...document.querySelector("#positions").children];
+    var arrayAnswers = [];
+    positionTags.forEach((tag)=>{
+        arrayAnswers.push(tag.textContent.replace(/ {4}|[\t\n\r]/gm,""));
     })
+    for (let tag = 0; tag < positionTags.length; tag++){
+        positionTags[tag].addEventListener("click", function(){
+            console.log("You clicked", positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,""));
+            if (positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,"").includes(getQuizQuestion())){
+                alert("You are right!");
+                changeColor(positionTags[tag], 'green');
+            }else{
+                alert('Try again!');
+                changeColor(positionTags[tag], 'red');
+            }
+              
+        })
+    }
 }
 
 function getQuizQuestion(){
@@ -32,7 +66,7 @@ function getQuizQuestion(){
 }
 
 function compareInput(answersArray){
-    return answersArray.find(answer => answer == getQuizQuestion());
+    return answersArray.find(answer => answer.includes(getQuizQuestion()));
 }
 
 function checkAnswer(){
@@ -41,5 +75,17 @@ function checkAnswer(){
     positionTags.forEach((tag)=>{
         arrayAnswers.push(tag.textContent.replace(/ {4}|[\t\n\r]/gm,""));
     })
-    console.log(compareInput(arrayAnswers));
+    for (let tag = 0; tag < positionTags.length; tag++){
+        positionTags[tag].addEventListener("click", function(){
+            console.log("You clicked", positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,""));
+            if (positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,"").includes(getQuizQuestion())){
+                alert("You are right!");
+                return;
+            }else{
+                alert('Try again!');
+                return;
+            }
+              
+        })
+    }
 }
