47488d68d0d3307428dadccabd1ba26b461b1bc6
Mateo Estrada <mateoestrada@estradma-mbp16.home>
2022-02-01 16:59:52 -0800

    Added function to remove positions that have already been guessed in the game from the positions array.

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-
-var helloWorld = "Baseball Quiz";
 var positions = ['Catcher', 'Pitcher', 'First Base', 'Second Base',  'Third Base', 'Shortstop', 'Right Field', 'Center Field', 'Left Field'];
-
-
+var answers = {
+                'question1': true, 
+                'question2': true, 
+                'question3': true, 
+                'question4': true, 
+                'question5': true, 
+                'question6': true,
+                'question7': true,
+                'question8': true,
+                'question9': true,
+            }
 
 // Begins the process
 window.onload = () => {
-    beginQuiz();
+    askPosition();
     addCircleEvents();
 }
 
-
-// HEADER Section
+// Button Section
 var header = document.getElementById("homeHeader");
-header.textContent = helloWorld;
+header.textContent = "Baseball Game";
 
 var myButton = document.querySelectorAll("button")[0];
 myButton.onclick = () => {
-    beginQuiz();
+    askPosition();
     addCircleEvents();
 }
 
@@ -27,36 +33,55 @@ myButton.onclick = () => {
 }
 
 
+function getRandomNumber(min, max) {
+    let totalEle = max - min + 1;
+    let result = Math.floor(Math.random() * totalEle) + min;
+    return result;
+}
 
 // Function Section
-function beginQuiz(){
+function askPosition(){
     var quizQuestions = document.querySelector("p");
-    quizQuestions.textContent = positions[Math.floor(Math.random()*positions.length)];
+    quizQuestions.textContent = positions[getRandomNumber(0, positions.length)];
     return quizQuestions.textContent;
 }
 
-function changeColor(tag, color){
-    console.log(tag.firstElementChild);
+function setAnswer(bool){
+    
+}
+function countAnswers(color, index){
+    if (color == 'red'){
+        console.log(index);
+        setAnswer(false);
+    }else if (color == 'green'){
+        console.log(index);
+        setAnswer(true);
+    }
+}
+
+function changeColor(tag, color, index){
+    countAnswers(color, index);
     tag.firstElementChild.style.fill = `${color}`;
+}
+
+function removePosition(position){
+    positions;
 }
 
 function addCircleEvents(){
     var positionTags = [...document.querySelector("#positions").children];
-    var arrayAnswers = [];
-    positionTags.forEach((tag)=>{
-        arrayAnswers.push(tag.textContent.replace(/ {4}|[\t\n\r]/gm,""));
-    })
     for (let tag = 0; tag < positionTags.length; tag++){
         positionTags[tag].addEventListener("click", function(){
             console.log("You clicked", positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,""));
             if (positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,"").includes(getQuizQuestion())){
-                alert("You are right!");
-                changeColor(positionTags[tag], 'green');
+                changeColor(positionTags[tag], 'green', tag);
+                removePosition(getQuizQuestion());
+                askPosition();
             }else{
-                alert('Try again!');
-                changeColor(positionTags[tag], 'red');
+                removePosition(getQuizQuestion());
+                changeColor(positionTags[tag], 'red', tag);
+                askPosition();
             }
-              
         })
     }
 }
