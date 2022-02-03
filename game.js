25e776dee16aa01ecc0c2c0b7aadb519540267a4
Mateo Estrada <mateoestrada@estradma-mbp16.home>
2022-02-03 13:26:19 -0800

    Refactored my onclick event logic.

--- game.js
+++ game.js
@@ -27,27 +27,52 @@ class Base{
 
 
 class GameState{
-    constructor(baseballField){
-        this.baseballField = baseballField;
+    constructor(){
+        this.basesArray = [];
+        this.field;
+        this.gameState = false;
     }
 
-    get currentValues(){
-        return this.currentValue;
+    get fieldObject(){
+        return this.field;
     }
-    setCurrentQuestion(){
-        var quizQuestions = document.querySelector("p");
-        quizQuestions.textContent = '';
-        quizQuestions.textContent = this.baseballField.basesArray[this.baseballField.basesArray.length - 1].baseName;
+    get allBaseballBases(){
+        return this.basesArray;
     }
+    // setCurrentQuestion(){
+    //     var quizQuestions = document.querySelector("p");
+    //     quizQuestions.textContent = '';
+    //     quizQuestions.textContent = this.baseballField.basesArray[this.baseballField.basesArray.length - 1].baseName;
+    // }
+    /**
+    * @param {any} base
+    */
+    set baseballBases(base){
+        this.basesArray.push(base);
+    }
+
+    /**
+    * @param {any} field
+    */
+       set baseballField(field){
+        this.field = field;
+    }
+
+    /**
+     * @param {any} state
+     */
+    set gameStates(state){
+        this.gameState = state;
+    }
+
+
 
 }
 
 class BaseballField{
     
     constructor(){
-        this.bases = ['Catcher', 'Pitcher', 'First Base', 'Second Base',  'Third Base', 'Shortstop', 'Right Field', 'Center Field', 'Left Field'];
-        this.basesArray = [];
-        this.currentValue = '';
+        this.currentQuestionValue = '';
     }
 
     // GET SECTION
@@ -56,42 +81,35 @@ class BaseballField{
     }
 
     get answer(){
-        // return this.answers[this.randomNumber(0, this.answers.length - 1)];
         return this.bases[this.bases.length-1];
     }
 
-    get allBaseballBases(){
-        return this.basesArray;
-    }
-
     get currentQuestion(){
-        return this.currentValue;
+        return this.currentQuestionValue;
     }
 
-    /**
-     * @param {any} base
-     */
-    set baseballBases(base){
-        this.basesArray.push(base);
+    // SET
+    set currentQuestion(value){
+        this.currentQuestionValue = value;
     }
 
     // MEHTODS
-    circleEvents(){
-        let answer = this.bases[this.bases.length-1];
-        let positionTags = [...document.querySelector("#positions").children];
-        let bases = this.bases;
-        for (let tag = 0; tag < positionTags.length; tag++){
-            positionTags[tag].addEventListener("click", function(event){
-                event.preventDefault();
-                let clickedPosition = positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,"");
-                if (clickedPosition.includes(answer)){
-                    changeState(positionTags[tag], 'green', answer, bases);
-                }else{
-                    changeState(positionTags[tag], 'red', answer, bases);
-                }
-            })
-        }
-    }
+    // circleEvents(){
+    //     let answer = this.bases[this.bases.length-1];
+    //     let positionTags = [...document.querySelector("#positions").children];
+    //     let bases = this.bases;
+    //     for (let tag = 0; tag < positionTags.length; tag++){
+    //         positionTags[tag].addEventListener("click", function(event){
+    //             event.preventDefault();
+    //             let clickedPosition = positionTags[tag].textContent.replace(/ {4}|[\t\n\r]/gm,"");
+    //             if (clickedPosition.includes(answer)){
+    //                 changeState(positionTags[tag], 'green', answer, bases);
+    //             }else{
+    //                 changeState(positionTags[tag], 'red', answer, bases);
+    //             }
+    //         })
+    //     }
+    // }
    
 }
 function changeColor(tag, color){
@@ -142,27 +160,26 @@ function listeningToBases(bases, currentValue){
 }
 
 window.onload = () => {
-    let bases = ['Catcher', 'Pitcher', 'First Base', 'Second Base',  'Third Base', 'Shortstop', 'Right Field', 'Center Field', 'Left Field'];
-    // Add the event listener to the bases.
-    let baseballQuiz = new BaseballField();
-    // baseballQuiz.circleEvents();
-
-    // Add the bases to the gameState
-    bases.forEach((base)=>{
-        baseballQuiz.baseballBases = new Base(base);
+    let positionTags = [...document.querySelector("#positions").children];
+    let baseballField = new BaseballField();
+    let gameState = new GameState();
+    positionTags.map((tag) =>{
+        tag.addEventListener("click", (event)=>{
+            event.preventDefault();
+            main(baseballField, gameState);
+        });
     })
-    // Begin the Game logic
-    let gameState = new GameState(baseballQuiz);
-    gameState.setCurrentQuestion();
-    // Listen to the document for any click events on the bases.
-
-    // I need to feed the baseballfield and the bases to my gamestate.
-    // Set the game state value
-    
-    listeningToBases(baseballQuiz.allBaseballBases, document.querySelector("p").textContent);
 }
 
 
 
-
+function main(gameState, baseballField){
+    let bases = ['Catcher', 'Pitcher', 'First Base', 'Second Base',  'Third Base', 'Shortstop', 'Right Field', 'Center Field', 'Left Field'];
+    // Feed the GameState the baseballField and the Base
+    bases.forEach((base)=>{
+        gameState.baseballBases = new Base(base);
+    })
+    gameState.baseballField = baseballField;
+    console.log(gameState);
+}
 module.exports = BaseballQuiz;
